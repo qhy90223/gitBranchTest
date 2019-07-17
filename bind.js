a0e1dfc328221d55331bd8f58551b5d108717ff3
秦焕禹 <qinhuanyu@viewhigh.com>
2019-07-17 18:05:14 +0800

    bind call

---
+++ bind.js
@@ -0,0 +1,15 @@
+Function.prototype.myBind = function(context){
+    this.fn=context
+    const args = Array.slice(1)
+    return fn(...args)
+}
+function test(key){
+    console.log(this);
+    
+    return key
+}
+const b={
+    a:1,
+    b:2
+}
+test.myBind(b,3333)
